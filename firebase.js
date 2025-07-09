7a18c9133e94502baef759936a8f1222c6d5a483
igamanraj <amanra812727@gmail.com>
2025-07-09 14:07:14 +0530

    Firebase.js updated

--- firebase.js
+++ firebase.js
@@ -2,9 +2,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signInWithPopup } from "firebase/auth";
 
+const VITE_APP_FIREBASE_API_KEY = import.meta.env.VITE_APP_FIREBASE_API_KEY;
+const VITE_APP_FIREBASE_AUTH_DOMAIN = import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCAFsBdeXFKa-eI7X0TIkl4rV1_BDc57cQ",
-  authDomain: "nanotech-2fe77.firebaseapp.com",
+  apiKey: VITE_APP_FIREBASE_API_KEY,
+  authDomain: VITE_APP_FIREBASE_AUTH_DOMAIN,
   // Copy from Firebase project settings
 };
 
